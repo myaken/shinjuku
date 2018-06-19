e6f70fa3441e6717eb7f5953738abccdc76d345c
mishitaka <40345184+mishitaka@users.noreply.github.com>
2018-06-19 15:54:43 +0900

    追加

---
+++ R/project/server.R
@@ -0,0 +1,47 @@
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$contents <- renderTable({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    
+    if(input$disp == "head") {
+      return(head(df))
+    }
+    else {
+      return(df)
+    }
+    
+  })  
+
+  output$contents2 <- renderPlot({
+    
+    # input$file1 will be NULL initially. After the user selects
+    # and uploads a file, head of that data file by default,
+    # or all rows if selected, will be shown.
+    
+    req(input$file1)
+    
+    df <- read.csv(input$file1$datapath,
+                   header = input$header,
+                   sep = input$sep,
+                   quote = input$quote)
+    
+    if(input$disp == "head") {
+    }
+    else {
+        plot(x = rnorm(df), y = rnorm(df))
+    }
+    
+  })  
+})
